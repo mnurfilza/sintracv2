968d7b0783c2abc7f6d865baf04e0701d0063f8d
Muhamad Nurfilza <choirfilza@gmail.com>
2024-12-09 00:14:04 +0700

    add cic d

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            // UserSeeder::class,
-            // CategorySeeder::class,
-            // SubCategorySeeder::class,
-            // BrandSeeder::class,
-            // ProductSeeder::class
+            UserSeeder::class,
+            CategorySeeder::class,
+            SubCategorySeeder::class,
+            BrandSeeder::class,
+            ProductSeeder::class,
             ProductStorageSeeder::class
 
         ]);
