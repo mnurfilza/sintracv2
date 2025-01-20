738abd299b66325133373373ae0f752e49cb4d96
Muhamad Nurfilza <choirfilza@gmail.com>
2025-01-20 08:20:11 +0700

    new meta tags for google indexing

--- resources/views/components/meta-tags.blade.php
+++ resources/views/components/meta-tags.blade.php
@@ -1,19 +1,11 @@
-
-<meta name="keywords" content="creative tim, updivision, html dashboard, laravel, html css dashboard laravel, soft ui dashboard laravel, laravel soft ui dashboard, soft ui admin, laravel dashboard, laravel admin, web dashboard, bootstrap 5 dashboard laravel, bootstrap 5, css3 dashboard, bootstrap 5 admin laravel, soft ui dashboard bootstrap 5 laravel, frontend, responsive bootstrap 5 dashboard, soft ui dashboard, soft ui laravel bootstrap 5 dashboard" />
-<meta name="description" content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
-<meta itemprop="name" content="Soft UI Dashboard Laravel by Creative Tim & UPDIVISION" />
-<meta itemprop="description" content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
-<meta itemprop="image" content="https://s3.amazonaws.com/creativetim_bucket/products/602/original/soft-ui-dashboard-laravel.jpg" />
-<meta name="twitter:card" content="product" />
-<meta name="twitter:site" content="@creativetim" />
-<meta name="twitter:title" content="Soft UI Dashboard Laravel by Creative Tim & UPDIVISION" />
-<meta name="twitter:description" content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
-<meta name="twitter:creator" content="@creativetim" />
-<meta name="twitter:image" content="https://s3.amazonaws.com/creativetim_bucket/products/602/original/soft-ui-dashboard-laravel.jpg" />
-<meta property="fb:app_id" content="655968634437471" />
-<meta property="og:title" content="Soft UI Dashboard Laravel by Creative Tim & UPDIVISION" />
-<meta property="og:type" content="article" />
-<meta property="og:url" content="https://www.creative-tim.com/live/soft-ui-dashboard-laravel" />
-<meta property="og:image" content="https://s3.amazonaws.com/creativetim_bucket/products/602/original/soft-ui-dashboard-laravel.jpg" />
-<meta property="og:description" content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
-<meta property="og:site_name" content="Creative Tim" />
+<meta name="keywords" content="sewa alat berat, penyedia alat berat, rental excavator murah, jasa alat berat profesional, penyewaan crane, harga alat berat terbaik, sewa bulldozer harian, alat berat proyek konstruksi, sewa alat berat terpercaya, alat berat Indonesia" />
+<meta name="description" content="Sewa alat berat terpercaya untuk proyek konstruksi dan infrastruktur. Harga terbaik, layanan cepat, dan alat berat berkualitas tinggi. Hubungi sekarang untuk penawaran khusus!" />
+<meta name="robots" content="index, follow" />
+<meta name="author" content="Penyedia Alat Berat Indonesia" />
+<meta itemprop="name" content="Sewa Alat Berat - Harga Terbaik & Layanan Cepat" />
+<meta itemprop="description" content="Penyedia alat berat profesional untuk proyek konstruksi Anda. Sewa excavator, crane, dan bulldozer dengan layanan terbaik dan harga bersaing!" />
+<meta property="og:title" content="Sewa Alat Berat Terpercaya - Harga Terbaik untuk Konstruksi" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://www.sintracindonesia.co.id" />
+<meta property="og:description" content="Layanan penyewaan alat berat terbaik di Indonesia. Sewa alat berat untuk konstruksi Anda sekarang juga dan dapatkan penawaran eksklusif!" />
+<meta property="og:site_name" content="Penyedia Alat Berat" />
