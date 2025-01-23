202a76ea72620d95d505caddc9a3b510f6a176d1
Muhamad Nurfilza <choirfilza@gmail.com>
2025-01-23 08:31:10 +0700

    add meta tag description

--- resources/views/components/meta-tags.blade.php
+++ resources/views/components/meta-tags.blade.php
@@ -1,12 +1,12 @@
-<meta name="keywords" content="sewa alat berat, penyedia alat berat, rental excavator murah, jasa alat berat profesional, penyewaan crane, harga alat berat terbaik, sewa bulldozer harian, alat berat proyek konstruksi, sewa alat berat terpercaya, alat berat Indonesia,sintrac indonesia, sintrac,sintrac,sintrac" />
-<meta name="description" content="Sewa alat berat terpercaya untuk proyek konstruksi dan infrastruktur. Harga terbaik, layanan cepat, dan alat berat berkualitas tinggi. Hubungi sekarang untuk penawaran khusus!" />
+<meta name="keywords" content="jual forkift, genset, excavator, wheel loader, motor grader, mini excavator, skid steer loader, crane dan lighting tower, toyota, mitsubishi forklift, sewa alat berat, penyedia alat berat, rental excavator murah, jasa alat berat profesional, penyewaan crane, harga alat berat terbaik, sewa bulldozer harian, alat berat proyek konstruksi, sewa alat berat terpercaya, alat berat Indonesia,sintrac indonesia, sintrac,sintrac,sintrac" />
+<meta name="description" content="jual forkift, genset, excavator, wheel loader, motor grader, mini excavator, skid steer loader, crane dan lighting tower, toyota, mitsubishi forklift, Sewa alat berat terpercaya untuk proyek konstruksi dan infrastruktur. Harga terbaik, layanan cepat, dan alat berat berkualitas tinggi. Hubungi sekarang untuk penawaran khusus!" />
 <meta name="robots" content="index, follow" />
-<meta name="author" content="Penyedia Alat Berat Indonesia" />
-<meta itemprop="name" content="Sewa Alat Berat - Harga Terbaik & Layanan Cepat" />
-<meta itemprop="description" content="Penyedia alat berat profesional untuk proyek konstruksi Anda. Sewa excavator, crane, dan bulldozer dengan layanan terbaik dan harga bersaing!" />
-<meta property="og:title" content="Sewa Alat Berat Terpercaya - Harga Terbaik untuk Konstruksi" />
+<meta name="author" content="PT Sintrac Niaga Indonesia" />
+<meta itemprop="name" content="PT Sintrac Niaga Indonesia" />
+<meta itemprop="description" content="jual forkift, genset, excavator, wheel loader, motor grader, mini excavator, skid steer loader, crane dan lighting tower, toyota, mitsubishi forklift, ,Penyedia alat berat profesional untuk proyek konstruksi Anda. Sewa excavator, crane, dan bulldozer dengan layanan terbaik dan harga bersaing!" />
+<meta property="og:title" content="PT Sintrac Niaga Indonesia" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="https://www.sintracindonesia.co.id" />
-<meta property="og:description" content="Layanan penyewaan alat berat terbaik di Indonesia. Sewa alat berat untuk konstruksi Anda sekarang juga dan dapatkan penawaran eksklusif!" />
-<meta property="og:site_name" content="Penyedia Alat Berat" />
+<meta property="og:url" content="https://www.sintracindonesia.co.id/" />
+<meta property="og:description" content="jual forkift, genset, excavator, wheel loader, motor grader, mini excavator, skid steer loader, crane dan lighting tower, toyota, mitsubishi forklift, Layanan penyewaan alat berat terbaik di Indonesia. Sewa alat berat untuk konstruksi Anda sekarang juga dan dapatkan penawaran eksklusif!" />
+<meta property="og:site_name" content="PT Sintrac Niaga Indonesia" />
 <meta name="google-site-verification" content="wMiqBdAPRsJk26DYLZZkc2czBbv4_yHq_RquDoQE3u8" />
